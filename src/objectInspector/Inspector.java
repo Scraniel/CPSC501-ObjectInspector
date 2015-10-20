8b55ab5248f5563dddf4d557da49e45b1e442dea
dglewis <dglewis@zone08-wa.cs.ucalgary.ca>
2015-10-20 17:07:37 -0600

    Adding superficial inspection on interfaces. Will revisite and potentially get rid of both findSuperclassInfo and findInterfaceInfo for a more generic findClassInfo method.

--- src/objectInspector/Inspector.java
+++ src/objectInspector/Inspector.java
@@ -16,19 +16,22 @@ public class Inspector implements ReflectiveInspector
 		Class toInspect = obj.getClass();
 		Class superClass = toInspect.getSuperclass();
 		
-		System.out.println("Class name:\n" + toInspect.getName());
-		System.out.println("Superclass Name:\n" + superClass.getName());
+		System.out.println("Class name:\n\t" + toInspect.getName());
+		System.out.println("Superclass Name:\n\t" + findSuperclassInfo(superClass));
+		System.out.print("Interfaces Implemented:\n" + findInterfaceInfo(toInspect));
 		
-		System.out.println("Fields:\n" + findDeclaredFields(toInspect));
+		
+		System.out.println("Fields:\n" + findDeclaredFieldInfo(toInspect));
 	}
 	
-	public String findDeclaredFields(Class toInspect)
+	public String findDeclaredFieldInfo(Class toInspect)
 	{
 		StringBuilder builder = new StringBuilder();
 		Field [] fields = toInspect.getDeclaredFields();
 		
 		for(Field field : fields)
 		{
+			builder.append('\t');
 			builder.append(field.getName());
 			builder.append('\n');
 		}
@@ -36,5 +39,31 @@ public class Inspector implements ReflectiveInspector
 		return builder.toString();
 		
 	}
+	
+	// Right now actually takes in the superclass as a parameter, consider
+	// changing to accept the subclass and getting the info that way. May 
+	// not need this class, depending on how the rest of the design goes.
+	//
+	// Potential solution: Just have an 'getClassInfo' method and call it
+	// when necessary for the class itself, it's interfaces, superclasses, etc.
+	public String findSuperclassInfo(Class toInspect)
+	{
+		return toInspect.getName();
+	}
 
+	public String findInterfaceInfo(Class toInspect)
+	{
+		StringBuilder builder = new StringBuilder();
+		Class [] interfaces = toInspect.getInterfaces();
+		
+		for(Class implementing : interfaces )
+		{
+			builder.append('\t');
+			builder.append(implementing.getName());
+			builder.append('\n');
+		}
+		
+		return builder.toString();
+	}
+	
 }
