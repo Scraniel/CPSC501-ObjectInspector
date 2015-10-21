1d9a026e1e5bc2168ba36df21839b6c6799066f3
dglewis <dglewis@zone43-wc.cs.ucalgary.ca>
2015-10-21 13:55:21 -0600

    Now gets superclass name, interfaces implemented, methods (including modifiers, return types, names and parameters), and fields (types and names). Working on getting recursion, modifiers / values for fields, and recursing up superclass hierarchy. NOTE: Already recurses up interfaces.

--- src/objectInspector/Inspector.java
+++ src/objectInspector/Inspector.java
@@ -1,9 +1,14 @@
 package objectInspector;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Inspector implements ReflectiveInspector 
 {
+	LinkedList<Class> toInspect = new LinkedList<Class>();
+	HashSet<String> inspected = new HashSet<String>();
 
 	public static void main(String[] args)
 	{
@@ -13,26 +18,33 @@ public class Inspector implements ReflectiveInspector
 	@Override
 	public void inspect(Object obj, boolean recursive) 
 	{
-		Class toInspect = obj.getClass();
-		Class superClass = toInspect.getSuperclass();
+		toInspect.add(obj.getClass());		 
 		
-		System.out.println("Class name:\n\t" + toInspect.getName());
-		System.out.println("Superclass Name:\n\t" + findSuperclassInfo(superClass));
-		System.out.print("Interfaces Implemented:\n" + findInterfaceInfo(toInspect));
+		while(!toInspect.isEmpty())
+		{
+			Class inspecting = toInspect.pop();
+			inspected.add(inspecting.getCanonicalName());
+			
+			System.out.println(findClassInfo(inspecting, recursive));
+			
+		}
 		
 		
-		System.out.println("Fields:\n" + findDeclaredFieldInfo(toInspect));
 	}
 	
-	public String findDeclaredFieldInfo(Class toInspect)
+	public String findDeclaredFieldInfo(Class inspecting)
 	{
 		StringBuilder builder = new StringBuilder();
-		Field [] fields = toInspect.getDeclaredFields();
+		Field [] fields = inspecting.getDeclaredFields();
 		
 		for(Field field : fields)
 		{
 			builder.append('\t');
+			builder.append(field.getType().getCanonicalName());
+			builder.append(' ');
 			builder.append(field.getName());
+			builder.append(" = ");
+			//builder.append(field.get(toInspect));
 			builder.append('\n');
 		}
 		
@@ -40,29 +52,90 @@ public class Inspector implements ReflectiveInspector
 		
 	}
 	
-	// Right now actually takes in the superclass as a parameter, consider
-	// changing to accept the subclass and getting the info that way. May 
-	// not need this class, depending on how the rest of the design goes.
-	//
-	// Potential solution: Just have an 'getClassInfo' method and call it
-	// when necessary for the class itself, it's interfaces, superclasses, etc.
-	public String findSuperclassInfo(Class toInspect)
-	{
-		return toInspect.getName();
-	}
-
-	public String findInterfaceInfo(Class toInspect)
+	public String findDeclaredMethodInfo(Class inspecting)
 	{
 		StringBuilder builder = new StringBuilder();
-		Class [] interfaces = toInspect.getInterfaces();
+		Method [] methods = inspecting.getDeclaredMethods();
+		
+		for(Method method : methods)
+		{
+			builder.append('\n');
+			builder.append('\t');
+			// modifiers
+			String modifier = Modifier.toString(method.getModifiers());
+			builder.append(modifier);
+			
+			// Return type
+			builder.append(' ');
+			builder.append(method.getReturnType().getCanonicalName());
+			
+			// Name
+			builder.append(' ');
+			builder.append(method.getName());
+			
+			// Arguments
+			builder.append('(');
+			Class [] parameters = method.getParameterTypes();
+			Class parameter;
+			for(int i = 0; i < parameters.length; i++)
+			{
+				parameter = parameters[i];
+				builder.append(parameter.getCanonicalName());
+				
+				// Commas between every parameter but the last one
+				if(i < parameters.length -1)
+				{
+					builder.append(", ");
+				}
+			}
+			builder.append(')');
+			
+			// Exceptions
+			Class [] exceptions = method.getExceptionTypes();
+			if(exceptions.length > 0){
+				builder.append(" throws ");
+				for(Class exception : exceptions)
+				{
+					builder.append(exception.getCanonicalName());
+					builder.append(' ');
+				}
+			}
+		}
+		
+		return builder.toString();
+	}
+
+	public String findInterfaceInfo(Class inspecting)
+	{
+		StringBuilder builder = new StringBuilder();
+		Class [] interfaces = inspecting.getInterfaces();
+		
 		
 		for(Class implementing : interfaces )
 		{
+			toInspect.add(implementing);
 			builder.append('\t');
 			builder.append(implementing.getName());
 			builder.append('\n');
 		}
 		
+		return builder.toString();
+	}
+	
+	public String findClassInfo(Class inspecting, boolean recursive)
+	{
+		StringBuilder builder = new StringBuilder();
+		
+		Class superClass = inspecting.getSuperclass();
+		String superClassName = superClass == null ? null : superClass.getCanonicalName();
+		
+		
+		builder.append("\n-----------------------------------------------------------\nNEW CLASS:\n\t" + inspecting.getCanonicalName());
+		builder.append("\nSuperclass Name:\n\t" + superClassName);
+		builder.append("\nInterfaces Implemented:\n" + findInterfaceInfo(inspecting));
+		builder.append("\nMethods:" + findDeclaredMethodInfo(inspecting));
+		builder.append("\nFields:\n" + findDeclaredFieldInfo(inspecting));
+		builder.append("\n-----------------------------------------------------------\n");
 		return builder.toString();
 	}
 	
